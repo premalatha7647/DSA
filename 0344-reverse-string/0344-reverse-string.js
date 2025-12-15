8a33be0c74b6e15912f11e942b55cb04cfa7a25a
Premalatha <169128822+premalatha7647@users.noreply.github.com>
2025-12-15 10:21:01 +0530

    Time: 1 ms (53.07%), Space: 61.2 MB (38.37%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.js
+++ 0344-reverse-string/0344-reverse-string.js
@@ -4,7 +4,7 @@
  */
 var reverseString = function (s) {
 
-    for (let i = 0; i < (s.length - 1) / 2; i++) {
+    for (let i = 0; i < s.length  / 2; i++) {
         let temp = s[i]
         s[i] = s[s.length - i - 1]
         s[s.length - i - 1] = temp
