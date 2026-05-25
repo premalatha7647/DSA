66531738a28bef480439cff82128e8c2c06ea57a
Premalatha <169128822+premalatha7647@users.noreply.github.com>
2026-05-25 16:02:31 +0530

    Time: 0 ms (100%), Space: 52.3 MB (98.92%) - LeetHub - Time: O(n), Space: O(1)

--- 0075-sort-colors/0075-sort-colors.js
+++ 0075-sort-colors/0075-sort-colors.js
@@ -3,47 +3,25 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var sortColors = function (nums) {
-    sort(nums, 0, nums.length - 1);
-    return nums
-};
+    let low = 0,
+        mid = 0,
+        high = nums.length - 1;
 
-function sort(nums, low, high) {
-    if (low >= high) return;
-    let mid = Math.floor((low + high) / 2);
-
-    sort(nums, low, mid);
-    sort(nums, mid + 1, high);
-    merge(nums, low, mid, high);
-}
-
-function merge(nums, low, mid, high) {
-    let temp = [];
-    let left = low;
-    let right = mid + 1;
-
-    while (left <= mid && right <= high) {
-        if (nums[left] <= nums[right]) {
-            temp.push(nums[left]);
-            left++;
+    while (mid <= high) {
+        if (nums[mid] === 0) {
+            [nums[mid], nums[low]] = [nums[low], nums[mid]];
+            low++;
+            mid++;
+        } else if (nums[mid] === 1) {
+            mid++;
         } else {
-            temp.push(nums[right]);
-            right++;
+            [nums[mid], nums[high]] = [nums[high], nums[mid]];
+            high--;
         }
     }
+};
 
-    while (left <= mid) {
-        temp.push(nums[left]);
-        left++;
-    }
-    while (right <= high) {
-        temp.push(nums[right]);
-        right++;
-    }
 
-    for (let i = low; i <= high; i++) {
-        nums[i] = temp[i - low];
-    }
-}
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
