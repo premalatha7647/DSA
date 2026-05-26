86abee2f11c60dbc4242ed335aa6b80c8ad2e06c
Premalatha <169128822+premalatha7647@users.noreply.github.com>
2026-05-26 07:18:17 +0530

    Time: 7 ms (25.33%), Space: 56.9 MB (57.91%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 0169-majority-element/0169-majority-element.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function (nums) {
+    const map = new Map()
+    const n = nums.length
+    let maxElement = 0
+    let count = 0
+    for (let i = 0; i < n; i++) {
+        map.set(nums[i], (map.get(nums[i]) || 0) + 1)
+    }
+
+    for (const [key, value] of map) {
+        if (value > count) {
+            count = value
+            maxElement = key
+        }
+    }
+    return maxElement
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
