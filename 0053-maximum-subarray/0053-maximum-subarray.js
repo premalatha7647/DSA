5d608842185f4fbabb79ff6c9202937ced0e0499
Premalatha <169128822+premalatha7647@users.noreply.github.com>
2026-05-28 22:27:02 +0530

    Time: 3 ms (43.29%), Space: 64 MB (41.59%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function (nums) {
+    let maxSum = -Infinity;
+    let n = nums.length;
+    let sum = 0;
+    for (let i = 0; i < n; i++) {
+        sum += nums[i];
+        maxSum = Math.max(maxSum, sum);
+        if (sum < 0) {
+            sum = 0;
+        }
+    }
+    return maxSum
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
