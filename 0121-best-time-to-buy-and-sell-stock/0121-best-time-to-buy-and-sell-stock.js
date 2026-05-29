e5a3f5207153e53a5db59bd2c60234cc0a60b051
Premalatha <169128822+premalatha7647@users.noreply.github.com>
2026-05-29 06:48:05 +0530

    Time: 68 ms (5.39%), Space: 63.6 MB (98.39%) - LeetHub - Time: O(n), Space: O(1)

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -4,17 +4,17 @@
  */
 var maxProfit = function (prices) {
     let min = prices[0];
-    let maxProfit = 0;
+    let profit = 0;
+    let n = prices.length;
+
     for (let i = 0; i < prices.length; i++) {
-
-        if (prices[i] - min > maxProfit) {
-            maxProfit = prices[i] - min
-        }
-        if (prices[i] < min) {
-            min = prices[i]
-        }
+        let cost = prices[i] - min;
+        profit = Math.max(profit, cost);
+        min = Math.min(min, prices[i]);
     }
-
-
-    return maxProfit;
+    return profit;
 }
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
